4acffba3e6c31e431adc651cf49f8109d30f9f49
Gustavolsl <gustavoluizsilvalima0209@gmail.com>
2019-02-14 10:29:36 -0200

    Gustavo

---
+++ LP2/Python exe.py
@@ -0,0 +1,23 @@
+# faça uma funcao ola_fulano, que recebe um nome e um adjetivo, 
+# e devolve a string "ola NOME, te acho ADJETIVO". 
+# Por exemplo, ola_fulano(joao,alto) devolve 
+# "ola joao, te acho alto"
+# dica: você pode juntar duas strings com o operador +
+# ex: "ola"+"mundo" resulta "olamundo"
+'''def ola_fulano(nome,adj):
+    return("ola "+nome+", te acho "+adj)'''
+def igual(a,b,c):
+    '''a = int(input("Digite um numero ")
+    b = int(input("Digite outro numero")
+    c = int(input("Digite mais um numero")'''
+    if a == b and a == c:
+        return "igual"
+    elif a == b and a != c or b == c and b != a or a == c and a != b:
+        return "dois iguais"
+    elif a != b and a != c:
+        return "diferente"
+    
+    
+    
+    
+        
